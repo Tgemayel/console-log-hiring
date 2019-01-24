d09b6964fa2b2f9ff2393a85309c20e9df4cdad5
Toni Gemayel <tonigemayel@gmail.com>
2019-01-24 15:43:45 -0800

    Create console.js

---
+++ console.js
@@ -0,0 +1,27 @@
+const MESSAGE = `
+
+         ______ _____ _____ __  __          
+        |  ____|_   _/ ____|  \/  |   /\    
+        | |__    | || |  __| \  / |  /  \            Passionate about design? Come work at Figma!
+        |  __|   | || | |_ | |\/| | / /\ \                    https://figma.com/careers/
+        | |     _| || |__| | |  | |/ ____ \ 
+        |_|    |_____\_____|_|  |_/_/    \_\                                                                                                                                                                                                   
+                                                                                                                                       
+
+
+`;
+
+export const notifyConsole = () => {
+  const isCsr = typeof window !== 'undefined';
+  const shouldLog =
+    isCsr &&
+    window.console &&
+    typeof console.log === 'function' &&
+    window.location.hostname !== 'figma.com';
+
+  if (!shouldLog) {
+    return;
+  }
+
+  console.log(MESSAGE);
+};
